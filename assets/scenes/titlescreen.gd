fc2aee7b074ec94b5b8ccdafd05c2970dc98c235
Nrojt <74906283+Nrojt@users.noreply.github.com>
2022-03-01 07:56:32 +0100

    Delete assets directory

--- assets/scenes/titlescreen.gd
+++
@@ -1,7 +0,0 @@
-extends Node2D
-
-func _process(delta):
-	if (Input.is_action_just_pressed("move_up")):
-		get_tree().change_scene("res://assets/scenes/levels/level1.tscn")
-		get_node("/root/MusicController").switch = true
-		get_node("/root/MusicController").selectedSong = 1
